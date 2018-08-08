3ae0853d8253aa814f758fe4f524f5a03026dfe6
mallorymaher <mallory.maher@comcast.net>
2018-08-08 18:00:02 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,4 +1,3 @@
-
 summer_olympics = {Sydney: "2000", Athens: "2004", Beijing: "2008", London: "2012"}
 
 summer_olympics[:Altanta] = "1996" 
@@ -8,3 +7,5 @@ summer_olympics[:Altanta] = "1996"
 summer_olympics.each do |place, year|
  puts "The #{place} summer olympics took place in #{year}."
 end
+
+upcased_cities
